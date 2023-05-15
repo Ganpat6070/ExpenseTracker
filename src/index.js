d2f8b6efeda878ee4e9895da5fd7e11cb8158416
Ganpat6070 <191200116070@asoit.edu.in>
2023-05-15 11:46:18 +0530

    first

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import react from 'react';
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
